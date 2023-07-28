2c613afbe0c09d6ccb9ad8a23cd3de3c0396235e
tonykastaneda <tonykastaneda@gmail.com>
2023-07-27 20:15:42 -0700

    Update zsh.sh

--- zsh.sh
+++ zsh.sh
@@ -10,5 +10,5 @@ ln -s ~/.dotfiles/ZSH/.zsh ~/.zsh;
 ln -s ~/.dotfiles/ZSH/.zshrc ~/.zshrc;
 
 # FZF Install
-git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf; 
-~/.fzf/install;
+# git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf; 
+# ~/.fzf/install;
